3f09cbe15ac1a9301c075a4a1f5dbc32ab099031
Benjamin-Mwiti <66462102+Benjamin-Mwiti@users.noreply.github.com>
2021-02-25 23:45:48 +0300

    Debugged

--- src/Cities.js
+++ src/Cities.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import CityContext from './Main';
+import { CityContext } from './Main';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,37 +10,52 @@ function Cities() {
     const [recently_Viewed, setRecently_Viewed] = useState("");
 
     const cityName = useContext(CityContext);
+    console.log(cityName);
 
     let isOnline = window.navigator.onLine;
 
     const openWeatherMapURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=1cc7ad57a30f3ba7be0d6a9766a69562`;
     const openWeatherMapFlag = `http://openweathermap.org/images/flags/${country_Name.toLowerCase()}.png`;
+    console.log(openWeatherMapURL);
 
     useEffect(() => {
         $(function() {
             async function getOpenWeatherMapData() {
-                $.ajax({
-                    url: openWeatherMapURL,
-                    type: "GET",
-                    dataType: "json",
-                    success: await function(data) {
-                        if(isOnline) {
-                            setCountry_Name(data.sys.country);
-                            console.log(cityName)
-                            $(('.city__weatherInfo p').eq(0)).before(openWeatherMapFlag);
-                            $('.city__weatherInfo').html("<p>" + data.name + "</p><p>Feels like " + data.main.feels_like + " <sup>o</sup>C " + data.weather[0].description + "." + data.weather[0].main + "</p><p><img src=http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png />" + data.main.temp + "</p>");
-                        } else {
-                            alert("You're not online");
+                let response;
+                try {
+                    response = await $.ajax({
+                        url: openWeatherMapURL,
+                        type: "GET",
+                        dataType: "json",
+                        success: function(data) {
+                            if(isOnline) {
+                                setCountry_Name(data.sys.country);
+                                $(function() {
+                                // $(('.city__weatherInfo p').eq(0)).before(openWeatherMapFlag);
+                                $('.city__weatherInfo').html("<p>" + data.name + "</p><p>Feels like " + data.main.feels_like + " <sup>o</sup>C " + data.weather[0].description + "." + data.weather[0].main + "</p><p><img src=http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png />" + data.main.temp + "</p>");
+                                });
+                            } else {
+                                alert("You're not online");
+                            }
+                        },
+                        error: (err) => {
+                            console.log(err);
                         }
-                    },
-                    error: (err) => {
-                        console.log(err);
-                    }
-                });
+                    });
+                } catch(error) {
+                    console.log(error);
+                }
+                finally {
+                    return response;
+                }
             }
-            getOpenWeatherMapData();
+            getOpenWeatherMapData()
+                .then(res => {
+                    console.log(res);
+                    return res;
+                });
         });
-    }, []);
+    }, [cityName]);
 
     return (
         <div className="cities">
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Main.css';
-import Cities, { country_Name } from './Cities';
+import Cities from './Cities';
 
 export const CityContext = React.createContext();
 
@@ -11,43 +11,46 @@ function Main() {
     // const countryName = useContext(country_Name);
 
     return (
-        <div className="app">
-      <div className="app__bar">
-        <div className="app__title">
-          <span><strong>Home</strong></span>
-          <span><strong>Visited Cities</strong></span>
+      <div className="app">
+        <div className="app__bar">
+          <div className="app__title">
+            <span><strong>Home</strong></span>
+            <span><strong>Visited Cities</strong></span>
+          </div>
+          <div className="search__container">
+            <form>
+              <input type="search" name="city name" value={city_Name} placeholder="Name of your city" 
+                onChange = {e => {
+                  setCity_Name(e.target.value);
+                }} />
+              <button type="submit" onClick={(e) => {
+                if(city_Name == "") {
+                  alert("The field should not be empty");
+                }
+                e.preventDefault();
+              }} >Search</button>
+            </form>
+          </div>
         </div>
-        <div className="search__container">
-          <form>
-            <input type="search" name="city name" value={city_Name} placeholder="Name of your city" 
-              onChange = {e => {
-                setCity_Name(e.target.value);
-              }} />
-            <button type="submit" onClick={(e) => {
-              if(city_Name == "") {
-                alert("The field should not be empty");
-              }
-              e.preventDefault();
-            }} >Search</button>
-          </form>
-        </div>
-      </div>
-      <div className="app__body">
-        <div className="city__forecast">
+        <div className="app__body">
           <CityContext.Provider value={city_Name}>
             <Cities />
           </CityContext.Provider>
-          <span></span>
-          <div className="city__stats">
+          <div className="city__forecast">
+            {/* <CityContext.Provider value={city_Name}>
+              <Cities />
+            </CityContext.Provider> */}
+            <span></span>
+            <div className="city__stats">
               <p>Toronto, ON</p>
               <p>Friday 12 February 2021</p>
               <p>-15<sup>o</sup>C</p>
               <p>Cloudy</p>
               <p>-13<sup>o</sup>C / -16<sup>o</sup>C</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     )
 }
 
